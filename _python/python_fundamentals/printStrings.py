ed0f10b2c6ee92275529a3bae2cdecdb23c3091a
Jesus <javarghas209@gmail.com>
2021-04-18 14:17:14 -0700

    Hello World asssignment

---
+++ _python/python_fundamentals/printStrings.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+
+hw = "Hello %s" % "world" 	# with literal values
+py = "I love Python %d" % 3 
+print(hw, py)
+# output: Hello world I love Python 3copy
+name = "Zen"
+age = 27
+print("My name is %s and I'm %d" % (name, age))		# or with variables
+# output: My name is Zen and I'm
