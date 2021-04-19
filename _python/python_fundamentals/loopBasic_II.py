1342ebcfc85f3f53ef0bcd59995d0911951fb6d5
Jesus <javarghas209@gmail.com>
2021-04-19 01:05:12 -0700

    loop assigment II

---
+++ _python/python_fundamentals/loopBasic_II.py
@@ -0,0 +1,2 @@
+#Biggie Size - Given a list, write a function that changes all positive numbers in the list to "big".
+#Example: biggie_size([-1, 3, 5, -5]) returns that same list, but whose values are now [-1, "big", "big", -5]
